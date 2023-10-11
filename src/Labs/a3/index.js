e7a0a684fdfda2021541ba2a84931da6974bc08b
Z-Langelaan <zoe.langelaan@casema.nl>
2023-10-11 01:03:09 -0400

    finished LAbs and setup Kanbas

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,10 +1,24 @@
 import JavaScript from "./JavaScript";
+import Add from "./Add";
+import PathParameters from "./PathParameters";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./todo/TodoList";
+import TodoItem from "./todo/TodoItem";
 
 function Assignment3() {
     return (
       <div className="container">
         <h1>Assignment 3</h1>
         <JavaScript/>
+        <Add/>
+        <PathParameters/>
+        <Classes/>
+        <Styles/>
+        <ConditionalOutput/>
+        <TodoItem/>
+        <TodoList/>
       </div>
     );
   }
